0c41aa7b77c121e854ec1bbbe86ec29b8e8d3b41
Roza Shakhbazyan <r.shakhbazyan@digitalpome.com>
2021-07-24 17:49:55 +0400

    Added auth implementations in infrastructure layer

---
+++ lib/infrastructure/auth/firebase_auth_facade.dart
@@ -0,0 +1,84 @@
+import 'package:dartz/dartz.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:injectable/injectable.dart';
+
+import '../../domain/auth/auth_failure.dart';
+import '../../domain/auth/i_auth_facade.dart';
+import '../../domain/auth/user.dart' as user;
+import '../../domain/auth/value_objects.dart';
+import '../../infrastructure/auth/firebase_user_mapper.dart';
+
+@LazySingleton(as: IAuthFacade)
+class FirebaseAuthFacade implements IAuthFacade {
+  final FirebaseAuth _firebaseAuth;
+  // final AuthLocalRepository _localRepository;
+
+  FirebaseAuthFacade(
+    this._firebaseAuth,
+    // this._localRepository,
+  );
+
+  @override
+  Future<Either<AuthFailure, Unit>> registerWithEmailAndPassword({
+    required EmailAddress emailAddress,
+    required Password password,
+  }) async {
+    final emailAddressString = emailAddress.getOrCrash();
+    final passwordString = password.getOrCrash();
+
+    try {
+      await _firebaseAuth.createUserWithEmailAndPassword(
+        email: emailAddressString,
+        password: passwordString,
+      );
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'email-already-in-use') {
+        return left(const AuthFailure.emailAlreadyInUse());
+      } else {
+        left(const AuthFailure.serverError());
+      }
+    }
+    return right(unit);
+  }
+
+  @override
+  Future<Either<AuthFailure, Unit>> signInWithEmailAndPassword({
+    required EmailAddress emailAddress,
+    required Password password,
+  }) async {
+    final emailAddressString = emailAddress.getOrCrash();
+    final passwordString = password.getOrCrash();
+
+    try {
+      await _firebaseAuth.signInWithEmailAndPassword(
+        email: emailAddressString,
+        password: passwordString,
+      );
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-found' || e.code == 'wrong-password') {
+        return left(const AuthFailure.invalidEmailAndPasswordCombination());
+      } else {
+        left(const AuthFailure.serverError());
+      }
+    }
+    return right(unit);
+  }
+
+  @override
+  Future<Option<user.User>> getSignedInUser() async =>
+      optionOf(_firebaseAuth.currentUser?.toDomain());
+
+  @override
+  Future<void> signOut() => _firebaseAuth.signOut();
+
+  // @override
+  // Future<void> autoSignIn() async {
+  //   final credentials = await _localRepository.getUserData();
+  //   credentials.fold(
+  //     (f) => unit,
+  //     (userCredential) => signInWithEmailAndPassword(
+  //         emailAddress: userCredential.emailAddress!,
+  //         password: userCredential.password!),
+  //   );
+  // }
+}
