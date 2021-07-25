152c63cf35226e0a8e79e11e29fb9425e165501e
Roza Shakhbazyan <r.shakhbazyan@digitalpome.com>
2021-07-25 22:35:39 +0400

    Minor changes

--- lib/injection.config.dart
+++ lib/injection.config.dart
@@ -5,21 +5,18 @@
 // **************************************************************************
 
 import 'package:firebase_auth/firebase_auth.dart' as _i3;
-import 'package:flutter_secure_storage/flutter_secure_storage.dart' as _i4;
 import 'package:get_it/get_it.dart' as _i1;
 import 'package:injectable/injectable.dart' as _i2;
-import 'package:shared_preferences/shared_preferences.dart' as _i9;
+import 'package:shared_preferences/shared_preferences.dart' as _i6;
 
-import 'application/auth/auth_module.dart' as _i10;
-import 'domain/auth/i_auth_facade.dart' as _i5;
-import 'domain/auth/i_auth_local_repository.dart' as _i7;
-import 'domain/core/i_localization_repository.dart' as _i11;
-import 'infrastructure/auth/auth_local_repository.dart' as _i8;
-import 'infrastructure/auth/firebase_auth_facade.dart' as _i6;
-import 'infrastructure/auth/firebase_injectable_module.dart' as _i13;
-import 'infrastructure/core/local_storage_injectable._module.dart' as _i14;
+import 'application/auth/auth_module.dart' as _i7;
+import 'domain/auth/i_auth_facade.dart' as _i4;
+import 'domain/core/i_localization_repository.dart' as _i8;
+import 'infrastructure/auth/firebase_auth_facade.dart' as _i5;
+import 'infrastructure/auth/firebase_injectable_module.dart' as _i10;
+import 'infrastructure/core/local_storage_injectable._module.dart' as _i11;
 import 'infrastructure/core/localization_repository.dart'
-    as _i12; // ignore_for_file: unnecessary_lambdas
+    as _i9; // ignore_for_file: unnecessary_lambdas
 
 // ignore_for_file: lines_longer_than_80_chars
 /// initializes the registration of provided dependencies inside of [GetIt]
@@ -31,23 +28,18 @@ Future<_i1.GetIt> $initGetIt(_i1.GetIt get,
       _$LocalStorageStorageInjectableModule();
   gh.lazySingleton<_i3.FirebaseAuth>(
       () => firebaseInjectableModule.firebaseAuth);
-  gh.lazySingleton<_i4.FlutterSecureStorage>(
-      () => localStorageStorageInjectableModule.storage);
-  gh.lazySingleton<_i5.IAuthFacade>(
-      () => _i6.FirebaseAuthFacade(get<_i3.FirebaseAuth>()));
-  gh.lazySingleton<_i7.ILocalAuthRepository>(
-      () => _i8.AuthLocalRepository(get<_i4.FlutterSecureStorage>()));
-  await gh.factoryAsync<_i9.SharedPreferences>(
+  gh.lazySingleton<_i4.IAuthFacade>(
+      () => _i5.FirebaseAuthFacade(get<_i3.FirebaseAuth>()));
+  await gh.factoryAsync<_i6.SharedPreferences>(
       () => localStorageStorageInjectableModule.prefs,
       preResolve: true);
-  gh.factory<_i10.AuthModule>(() =>
-      _i10.AuthModule(get<_i5.IAuthFacade>(), get<_i7.ILocalAuthRepository>()));
-  gh.lazySingleton<_i11.ILRepository>(
-      () => _i12.LRepository(get<_i9.SharedPreferences>()));
+  gh.factory<_i7.AuthModule>(() => _i7.AuthModule(get<_i4.IAuthFacade>()));
+  gh.lazySingleton<_i8.ILRepository>(
+      () => _i9.LRepository(get<_i6.SharedPreferences>()));
   return get;
 }
 
-class _$FirebaseInjectableModule extends _i13.FirebaseInjectableModule {}
+class _$FirebaseInjectableModule extends _i10.FirebaseInjectableModule {}
 
 class _$LocalStorageStorageInjectableModule
-    extends _i14.LocalStorageStorageInjectableModule {}
+    extends _i11.LocalStorageStorageInjectableModule {}
