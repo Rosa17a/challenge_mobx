d8d79fd4745d6c3b97f4ba060e6b1d9736e49cc5
Roza Shakhbazyan <r.shakhbazyan@digitalpome.com>
2021-07-25 14:29:53 +0400

    Updated routes

--- lib/presentation/routes/router.dart
+++ lib/presentation/routes/router.dart
@@ -1,8 +1,8 @@
 import 'package:auto_route/auto_route.dart';
 
-import '../home/home_page.dart';
-import '../sign_in/sign_in_page.dart';
-import '../splash/slpash_page.dart';
+import '../pages/home/home_page.dart';
+import '../pages/sign_in/sign_in_page.dart';
+import '../pages/splash/slpash_page.dart';
 
 @MaterialAutoRouter(
   routes: <AutoRoute>[
