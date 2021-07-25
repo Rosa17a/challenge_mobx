dccdd1fff0a1410d343810fce84f455ff4ee4852
Roza Shakhbazyan <r.shakhbazyan@digitalpome.com>
2021-07-25 19:29:05 +0400

    Added app colors

---
+++ lib/presentation/core/colors.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+/// Defines the color palette for App UI.
+abstract class AppColors {
+  /// White
+  static const Color white = Color(0xffffffff);
+
+  static const Color blackB = Color(0xff201a29);
+
+  /// Black
+  static const Color blackG = Color(0xFF202124);
+
+  /// Deep blue
+  static const Color blueDeep = Color.fromRGBO(12, 29, 48, 1);
+
+  /// GreyBlue for disable color
+  static const Color blueGrey = Color.fromRGBO(37, 51, 67, 1);
+
+  /// Purple
+  static const Color purple = Color.fromRGBO(30, 34, 52, 1);
+
+  /// Red
+  static const Color red = Color(0xffE22900);
+
+  /// Orange red
+  static const Color orangeRed = Color.fromRGBO(220, 103, 94, 1);
+
+  /// Orange red muted color
+  static const Color orangeRedMuted = Color.fromRGBO(123, 74, 77, 1);
+  // Transparent
+  static const Color transpatent = Colors.transparent;
+
+  static const Color akaroa = Color(0xffdeccb8);
+}
