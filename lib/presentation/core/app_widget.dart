c85b26e9a9792827eddd09256a5f6fefb309625b
Roza Shakhbazyan <r.shakhbazyan@digitalpome.com>
2021-07-25 20:06:15 +0400

    Done some changes in appwidget to handle language change

--- lib/presentation/core/app_widget.dart
+++ lib/presentation/core/app_widget.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/localizations.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
@@ -7,12 +6,12 @@ import 'package:provider/provider.dart';
 import '../../application/core/locale_store.dart';
 import '../../injection.dart';
 import '../routes/router.gr.dart';
+import 'themes.dart';
 
 class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final _router = AppRouter();
-
     return MultiProvider(
       providers: [
         Provider<LocaleStore>(create: (_) => getIt<LocaleStore>()..getLocale())
@@ -22,25 +21,7 @@ class AppWidget extends StatelessWidget {
           builder: (_) => MaterialApp.router(
             title: 'Challenge',
             debugShowCheckedModeBanner: false,
-            theme: ThemeData.light().copyWith(
-              primaryColor: Colors.green[800],
-              accentColor: Colors.blueAccent,
-              inputDecorationTheme: InputDecorationTheme(
-                // labelStyle: TextStyle(color: Colors.green[800]),
-
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(8),
-                  borderSide: BorderSide(color: Colors.green[800]!),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(8),
-                  borderSide: BorderSide(color: Colors.green[800]!),
-                ),
-              ),
-              floatingActionButtonTheme: FloatingActionButtonThemeData(
-                backgroundColor: Colors.blue[900],
-              ),
-            ),
+            theme: DefaultTheme.standard,
             locale: value.locale,
 
             // Localizations delegates.
